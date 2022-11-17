5468cd5066d7642c2b44e5f2d4a3616cdeb61268
misbahproject <mmisbahulmunir601@gmail.com>
2022-11-16 21:19:12 -0800

    add User Login  & Middleware

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,11 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-     {{-- <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
-
-    {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     
     <link rel="stylesheet" href="/css/bootstrap.min.css">
@@ -24,10 +19,6 @@
     <div class="container mt-5 pt-5">
         @yield('container')
     </div>
-
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script> --}}
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
